ee0effb7bc74ec6fd2fe1184744cf4fce2066805
FirMoveBrick <945184211@qq.com>
2019-11-22 21:16:12 +0800

    Update 实例讲解.py

--- Python_code/src/正则表达式/实例讲解.py
+++ Python_code/src/正则表达式/实例讲解.py
@@ -2,6 +2,6 @@ import re
 # 实例：正则匹配.com和.cn网址
 string="<a href='http://www.baidu.com'>百度首页</a>"
 pat="[a-zA-Z]+://[^\s]*[.com|.cn]"
-# 判断网址格式是否正常
+# 判断网址格式是否是正常
 result = re.compile(pat).findall(string)
 print(result)
